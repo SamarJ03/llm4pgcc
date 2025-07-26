c3817c14335741fa0f2b571cebefb7fdf4b8d838
SamarJ03 <155527341+SamarJ03@users.noreply.github.com>
2025-07-26 05:15:35 -0400

    feat(utils/input): Finished initial logic changes. Most likely will add more features in the future. Still have to debug.

--- data/info.md
+++ data/info.md
@@ -8,7 +8,7 @@ Place the source input data within this directory path. Accepted formats are CSV
 
 <FIELDS>
 - the default column names are listed in quotations.
-- to make alterations to the column names, run <init --set_labels>. 
+- to make alterations to the column names, run <init --set_labels>.
 
 - required:
   > "SMILES"
@@ -25,4 +25,26 @@ Place the source input data within this directory path. Accepted formats are CSV
   > "pathway"
   > "target"
   > "info"
+```
+
+```
+PATH LAYOUT PLAN FOR DATA/:
+
+source_lib.xlsx (user input csv or xlsx file)
+raw_lib.csv (user input in CSV format, only required params kept)
+clean_lib.csv (raw_lib.csv after data preprocessing steps)
+lib.json (JSON file of clean_lib.csv compounds with BOTH required and optional fields)
+features.csv (CSV of all compounds from clean_lib.csv and maccs/ecfp4/rdkit descriptors)
+features/
+ - ecfp4_meta.csv (ecfp4 descriptors prior to low-var removal)
+ - maccs_meta.csv (maccs descriptors prior to low-var removal)
+ - rdkit_meta.csv (rdkit descriptors prior to low-var removal)
+ - ridLowVar/ (low var descriptors removed)
+   - ecfp4.csv
+   - maccs.csv
+   - rdkit.csv
+scaffold/
+ - ecfp4/ (contains train/test/valid sets)
+ - maccs/ (contains train/test/valid sets)
+ - rdkit/ (contains subgroupings train/test/valid sets including file of all rdkit descriptors train/test/valid set)
 ```
--- utils/input.py
+++ utils/input.py
@@ -1,4 +1,5 @@
-import os, sys, argparse
+import os, argparse
+import pandas as pd
 from loguru import logger
 from dotenv import load_dotenv
 load_dotenv()
@@ -6,62 +7,43 @@ load_dotenv()
 class VerifyData:
     def __init__(self, initialParser: argparse.ArgumentParser):
         dataParser = initialParser.add_argument_group(title='input')
+        dataParser.add_argument('-sl', '--set_labels', type=dict[str,str], default=None, help="")
+        args, _ = dataParser.parse_known_args(['set_labels'])
+        self.set_labels = args.set_labels
+        self.sourceData_path = None
 
-        dataParser.add_argument('-sl', '--set_labels', type=dict[str:str], default=None, help="")
-        dataParser.add_argument('-vd', '--verify_data', action='store_true', help='')
+        dataPath = os.getenv("DATA_PATH")
+        if not os.exists(dataPath): logger.error(f'Incorrect path to source data:\n{dataPath}')
 
-        args, _ = dataParser.parse_known_args(['verify_data', 'set_labels'])
-        self.verify_path = args.verify_path
-        self.verify_data = args.verify_data
-        self.set_labels = args.set_labels if isinstance(args.set_labels, dict[str:str]) else False
-        self.data_path = args.data_path
+        df = None
+        for file in os.listdir(dataPath):
+            name = str(os.fsdecode(file))
+            if not name.endswith(['.csv', '.xlsx']) and name.startswith('source'): continue
+            path = os.path.join(os.getenv("DATA_PATH"), name)
+            df = pd.read_csv(path, nrows=0) if name.endswith('.csv') else pd.read_excel(path, nrows=0)
+        if not df: logger.error('No source data found in data/ directory..\n(Ensure that input file name begins with "source" and is either CSV or XLSX.)')
 
-    # PATH LAYOUT PLAN FOR DATA/:
+        if isinstance(self.set_labels, dict[str,str]): df = self.changeLabels(df)
 
-    # source_lib.xlsx (user input csv or xlsx file)
-    # raw_lib.csv (user input in CSV format, only required params kept)
-    # clean_lib.csv (raw_lib.csv after data preprocessing steps)
-    # lib.json (JSON file of clean_lib.csv compounds with BOTH required and optional fields)
-    # features.csv (CSV of all compounds from clean_lib.csv and maccs/ecfp4/rdkit descriptors)
-    # features/
-    #  - ecfp4_meta.csv (ecfp4 descriptors prior to low-var removal)
-    #  - maccs_meta.csv (maccs descriptors prior to low-var removal)
-    #  - rdkit_meta.csv (rdkit descriptors prior to low-var removal)
-    #  - ridLowVar/ (low var descriptors removed)
-    #    - ecfp4.csv
-    #    - maccs.csv
-    #    - rdkit.csv
-    # scaffold/
-    #  - ecfp4/ (contains train/test/valid sets)
-    #  - maccs/ (contains train/test/valid sets)
-    #  - rdkit/ (contains subgroupings train/test/valid sets including file of all rdkit descriptors train/test/valid set)
+        rawData3 = self.sortData(df)
+        rawData3.to_csv(os.path.join(os.getenv("DATA_PATH"), 'raw_lib.csv'))
 
 
-    # def verifyData(self):
-    #     dataPath = str(tempArgs['data_path'])
-    #     configPath = os.path.join(os.getcwd(), 'config')
+    def sortData(self, input: pd.DataFrame):
+        toInclude = ['compound_name'] if 'compound_name' in input.columns else []
+        toInclude.append('SMILES', 'PGCC_score')
+        toInclude.append(optCol for optCol in ['nonPGCC_score', 'pathway', 'target', 'info'] if optCol in input.columns)
+        return pd.DataFrame(input, columns=toInclude, copy=True)
 
-    #     if not os.path.exists(configPath):
-    #         logger.error('config/ directory path not found..')
-    #         return False, None
+    def changeLabels(self, input: pd.DataFrame):
+        df = pd.DataFrame(input, copy=True)
+        new_labels = {obs: exp for exp, obs in dict(self.set_labels).items()}
+        if 'SMILES' not in new_labels.keys() and 'SMILES' not in input.columns: logger.error('"SMILES" field not found in source data.')
+        if 'PGCC_score' not in new_labels.keys() and 'PGCC_score' not in input.columns: logger.error('"PGCC_score" field not found in source data.')
 
-    #     for file in os.listdir(configPath):
-    #         fileName = str(os.fsdecode(file))
-    #         if dataPath=="": pass
-    #             # if fileName.endswith(['.csv', '.xlsx']):
-    #             #     filePath = os.path.join(configPath, fileName)
-    #             #     import pandas as pd
-    #             #     dataCol = pd.read_csv(filePath, nrows=0).columns if fileName.endswith('.csv') else pd.read_excel(filePath, nrows=0).columns
-    #             # return verifyFileType()
-    #         elif dataPath.endswith(['.csv', '.xlsx']):
-    #             if fileName==dataPath: return True, os.path.join(configPath, fileName)
-    #             else:
-    #                 logger.error(f'No files match {dataPath} within config/')
-    #                 return False, None
-    #         else:
-    #             logger.error(f'Invalid --data_path entry:\n{dataPath}')
-    #             return False, None
-
-    # def verifyFileType(fileName: str, configPath: str):
-    #     filePath = os.path.join(configPath, fileName)
-    #     dataCol = pd.read_csv(filePath, nrows=0).columns if fileName.endswith('.csv') else pd.read_excel(filePath, nrows=0).columns
+        for obs, exp in new_labels.items():
+            if obs not in df.columns:
+                logger.info(f'{obs} not found in source data fields. Field rename skipped..')
+                continue
+            df.rename({obs:exp}, axis=1)
+        return df
